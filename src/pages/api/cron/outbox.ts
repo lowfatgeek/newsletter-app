714927627600d9127e5ef4470e630efc6dbfbf34
kelaswfa <proxyland@gmail.com>
2026-09-13 23:33:48 +0700

    fix: correct lib import paths in cron outbox api route

--- src/pages/api/cron/outbox.ts
+++ src/pages/api/cron/outbox.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
-import { env } from "../../lib/env";
-import { processOutbox } from "../../lib/mailworker";
+import { env } from "../../../lib/env";
+import { processOutbox } from "../../../lib/mailworker";
 
 export const GET: APIRoute = async ({ request }) => {
   if (request.headers.get("x-cron-secret") !== env("CRON_SECRET")) {
